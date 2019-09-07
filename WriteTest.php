48e748813de0f1fee548bb0b5693fb199b031fcb
Sandro Schaefer <aswe5017@gmail.com>
2019-09-07 15:54:28 +0000

    Fixed Random gen

--- WriteTest.php
+++ WriteTest.php
@@ -1,16 +1,15 @@
-<?php
 
 require __DIR__ . '/vendor/autoload.php';
-for ($i = 0; $i < 20; $i++) {
+for ($i = 0; $i < 5; $i++) {
     $pid = pcntl_fork();
     if (!$pid) {
-	while (true) {
-	$random = substr(str_shuffle("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"), 0, 32);
-	$random1 = substr(str_shuffle("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"), 0, 32);
-	$doc = array('$random'=>'$random1');
-	$conn = r\connect('192.168.188.26', 28015, 'test');
-	r\table("test")->insert($doc)->run($conn);
-	}
+        while (true) {
+        $random = array(substr(str_shuffle("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"), 0, 32));
+  $random1 = array(substr(str_shuffle("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"), 0, 32));
+        $doc = array($random[0], $random1[0]);
+        $conn = r\connect('192.168.188.26', 28015, 'test');
+        r\table("test")->insert($doc)->run($conn);
+        }
 exit();
     }
 }
--- test.php
+++ test.php
@@ -1,2 +1,17 @@
 <?php
-echo 1;
+
+require __DIR__ . '/vendor/autoload.php';
+for ($i = 0; $i < 5; $i++) {
+    $pid = pcntl_fork();
+    if (!$pid) {
+	while (true) {
+	$random = array(substr(str_shuffle("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"), 0, 32));
+  $random1 = array(substr(str_shuffle("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890"), 0, 32));
+	$doc = array($random[0], $random1[0]);
+	$conn = r\connect('192.168.188.26', 28015, 'test');
+	r\table("test")->insert($doc)->run($conn);
+	}
+exit();
+    }
+}
+
